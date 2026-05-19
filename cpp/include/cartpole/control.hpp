31cdbd7995234a4b3fe0f7361f58bf5a3c7cb616
amcaleer271 <amcaleer1127@gmail.com>
2026-05-19 14:53:35 -0400

    Added state space system for LQR, need to find a way to solve hamilton-jacobi-bellman equation

--- cpp/include/cartpole/control.hpp
+++ cpp/include/cartpole/control.hpp
@@ -1,13 +1,19 @@
 #pragma once
 #include <cartpole/state.hpp>
 #include <Eigen/Dense>
+#include <cartpole/params.hpp>
 
 namespace cartpole
 {
     class Controller{
+        private:
+            Eigen::MatrixXd A = Eigen::MatrixXd::Zero(4,4);
+            Eigen::MatrixXd B = Eigen::MatrixXd::Zero(4,1);
         public:
             virtual double control(State& x) = 0;
             virtual ~Controller() = default;
+
+            void set_system(Params& params);  //creates the A and B state space matricies
     };
 
     class PID : public Controller{
@@ -20,8 +26,13 @@ namespace cartpole
     };
 
     class LQR : public Controller{
+        private:
+            Eigen::MatrixXd Q;
+            Eigen:: MatrixXd R;
+
         public:
             double control(State& x);
+            LQR(Eigen::MatrixXd Q, Eigen::MatrixXd R);
     };
 
     class MPC : public Controller{
--- cpp/src/control.cpp
+++ cpp/src/control.cpp
@@ -2,7 +2,7 @@
 
 namespace cartpole
 {
-
+    //PID Constuctor, takes in gains
     PID::PID(Eigen::Vector2d kp, Eigen::Vector2d ki, Eigen::Vector2d kd){
         this->kp = kp;
         this->ki = ki;
@@ -11,6 +11,23 @@ namespace cartpole
 
     double PID::control(State& x){
         return kp(0)*x.x + kd(0)*x.x_dot + kp(1)*x.theta + kd(1)*x.theta_dot;
+    };
+
+    //create state space A and B matricies based on physical params
+    void Controller::set_system(Params& params){
+        this->A(0,1) = 1.0;
+        this->A(1,2) = -1.0 * params.g * params.m2 / params.m1;
+        this->A(2,3) = 1.0;
+        this->A(3,2) = params.g * (params.m1 + params.m2) / (params.L * params.m1);
+
+        this->B(1,0) = 1.0 / params.m1;
+        this->B(3,0) = -1.0 / (params.m1 * params.L);
+    };
+
+    //LQR Constructor, takes in gains
+    LQR::LQR(Eigen::MatrixXd Q, Eigen::MatrixXd R){
+        this->Q = Q;
+        this->R = R;
     };
 
     double LQR::control(State& x){
